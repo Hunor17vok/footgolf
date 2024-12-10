35e6f89fb2778edc030ae6009b38eef92ba23a31
Hunor17vok <163414670+Hunor17vok@users.noreply.github.com>
2024-12-10 12:44:19 +0100

    Add project files.

---
+++ footgolf/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace footgolf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Sportolo> Sportolok = new();
+            foreach (var sor in File.ReadAllLines("fob2016.txt"))
+            {
+                Sportolok.Add(new Sportolo(sor));
+            }
+            Console.WriteLine($"3.Feladat:A sportolok száma:{Sportolok.Count()}");
+
+            int C = 0;
+            foreach (var i in Sportolok)
+            {
+                if (i.Kategoria == "nok")
+                {
+                    C += 1;
+                }
+                
+            }
+            Console.WriteLine($"4.Feladat:A női versenyzök atánya:{Sportolok.Count() / C:0.0.0}%");
+        }
+    }
+}
---
+++ footgolf/Sportolo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace footgolf
+{
+    internal class Sportolo
+    {
+        public string Nev { get; set; }
+        public string Kategoria { get; set; }
+        public string Egyesulet { get; set; }
+        
+
+
+        public Sportolo(string sor)
+        {
+            string[] D = sor.Split(';');
+            Nev = D[0];
+            Kategoria = D[1];
+            Egyesulet = D[2];
+            
+        }
+    }
+}
